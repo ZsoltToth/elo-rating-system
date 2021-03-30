6b236b61303fb2a30c1a7ac46b90f40aecbf99ef
Zsolt Toth <tothzs87@gmail.com>
2021-03-30 17:13:22 +0200

    refactor: checkstyle violations were fixed

--- src/main/java/hu/iit/uni/miskolc/advanced/java/App.java
+++ src/main/java/hu/iit/uni/miskolc/advanced/java/App.java
@@ -12,6 +12,12 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class App implements CommandLineRunner {
 
+    private static final String ALICE = "Alice";
+
+    private static final String BOB = "Bob";
+
+    private static final String CHARLIE = "Charlie";
+
     @Autowired
     private ApplicationContext context;
 
@@ -23,18 +29,18 @@ public class App implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         PlayerManager playerManager = context.getBean(PlayerManager.class);
-        EloRatingService service = context.getBean(EloRatingService.class);
 
-        playerManager.register(new Player("Alice", 1000));
-        playerManager.register(new Player("Bob", 1000));
-        playerManager.register(new Player("Charlie", 1000));
+        playerManager.register(new Player(ALICE, 1000));
+        playerManager.register(new Player(BOB, 1000));
+        playerManager.register(new Player(CHARLIE, 1000));
 
         playerManager.fetchPlayers()
                 .forEach(System.out::println);
 
+        EloRatingService service = context.getBean(EloRatingService.class);
         service.updatePlayerRankings(
-                playerManager.fetchByName("Alice"),
-                playerManager.fetchByName("Bob")
+                playerManager.fetchByName(ALICE),
+                playerManager.fetchByName(BOB)
         );
         playerManager.fetchPlayers()
                 .forEach(System.out::println);
